0b7727e29d02aac4c94a6b1c585e20eff01c3bf2
박혜원 <79081836+phw-plain@users.noreply.github.com>
2022-03-31 23:44:36 +0900

    Android 디자인 업데이트 1

--- BarCodeReader/app/src/main/java/com/example/barcodereader/MainActivity.kt
+++ BarCodeReader/app/src/main/java/com/example/barcodereader/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.barcodereader
 
 import android.content.Intent
-import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
 import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val eventText: TextView = findViewById(R.id.eventText)
+        FadeInEvent(eventText);
+    }
+
+    fun FadeInEvent(view: View) {
+        YoYo.with(Techniques.FadeIn)
+            .duration(1000)
+            .repeat(-1)
+            .playOn(findViewById(R.id.eventText))
     }
 
     fun startBarcodeReader(view: View) {
@@ -21,8 +32,8 @@ class MainActivity : AppCompatActivity() {
 
     fun startBarcodeReaderCustom(view: View) {
         val integrator = IntentIntegrator(this)
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)   // 규격 설정(QR_CODE : QR만 찍음 => 속도 형식 ↑)
-        integrator.setPrompt("QR 코드를 스캔하여 주세요") // 스캔 및 문구
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.EAN_13)   // 규격 설정(QR_CODE : QR만 찍음 => 속도 형식 ↑)
+        integrator.setPrompt("바코드를 사각형 안에 비춰주세요.") // 스캔 및 문구
         integrator.setCameraId(0)   // 0=후면, 1=전면
         integrator.setBeepEnabled(true) // 스캔 후 소리
         integrator.setBarcodeImageEnabled(true) // 결과 뿐만 아닌 이미지도 전달
@@ -37,15 +48,12 @@ class MainActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
             }
-            if(result.barcodeImagePath != null) {
-                val bitmap = BitmapFactory.decodeFile(result.barcodeImagePath)
-                val scannedBitmap: ImageView = findViewById(R.id.scannedBitmap)
-                scannedBitmap.setImageBitmap(bitmap)
-            }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
+
+
 
 
 

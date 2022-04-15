f197a24d8effd7d79e6e12697dfad3593ab209d7
plainphw <s2028@e-mirim.hs.kr>
2022-04-15 15:10:12 +0900

    react 연동

--- workspace/javaweb/src/main/java/com/cos/market/javaweb/JavawebApplication.java
+++ workspace/javaweb/src/main/java/com/cos/market/javaweb/JavawebApplication.java
@@ -1,4 +1,4 @@
-package com.cos.market;
+package com.cos.market.javaweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- workspace/javaweb/src/test/java/com/cos/market/javaweb/JavawebApplicationTests.java
+++ workspace/javaweb/src/test/java/com/cos/market/javaweb/JavawebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cos.market;
+package com.cos.market.javaweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

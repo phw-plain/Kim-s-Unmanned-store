9ff5de94655dee84b491cf4f175cb91d4d9935c1
박혜원 <79081836+phw-plain@users.noreply.github.com>
2022-04-16 00:53:11 +0900

    react와 spring boot 연동 및 기본 화면 세팅

--- workspace/javaweb/src/main/webapp/reactjs/src/App.js
+++ workspace/javaweb/src/main/webapp/reactjs/src/App.js
@@ -1,35 +1,60 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import logo from './logo.svg';
 import './App.css';
+import { Navbar,Nav,NavDropdown,Button,Jumbotron } from 'react-bootstrap';
 
 function App() {
-  // 요청받은 정보를 담아줄 변수 선언
-  const [ testStr, setTestStr ] = useState('');
 
-  // 변수 초기화
-  function callback(str) {
-    setTestStr(str);
-  }
-
-  // 첫 번째 렌더링을 마친 후 실행
-  useEffect(
-      () => {
-        axios({
-            url: '/home',
-            method: 'GET'
-        }).then((res) => {
-            callback(res.data);
-        })
-      }, []
-  );
-
-  return (
-      <div className="App">
-          <header className="App-header">
-              {testStr}
-          </header>
+  return(
+      // <div className="App">
+      // <header className="App-header">
+      //   <h1>Hello React!!</h1>
+      // </header>
+      // </div>
+      <div>
+        <Jumbotron class="background"></Jumbotron>
+        <Container class="background"></Container>  
       </div>
   );
+
+  function Jumbotron() {
+    return (
+    <div class="jumbotron">
+      <h1>박리다매 무인가게</h1>
+      <p>
+        환영합니다.
+      </p>
+      <Button variant='primary'>계산하기</Button>
+    </div>
+    );
+  }
+
+  function Container() {
+    return (
+      <div className='container'>
+      <div className='row'>
+        <div className='col-md-4'>
+          <img src="https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201812022340" width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명&가격</p>
+        </div>
+        <div className='col-md-4'>
+          <img src="https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201812022340" width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명&가격</p>
+        </div>
+        <div className='col-md-4'>
+          <img src="https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201812022340" width="100%"/>
+          <h4>상품명</h4>
+          <p>상품설명&가격</p>
+        </div>
+      </div>
+    </div>        
+    );
+  }
+ 
+
+
 }
 
 export default App;

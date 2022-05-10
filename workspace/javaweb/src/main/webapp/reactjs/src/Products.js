be979fc600d464392c52614c2242d02063f06bc4
plainphw <s2028@e-mirim.hs.kr>
2022-05-10 13:22:56 +0900

    상품 검색 페이지 카테고리 및 검색 ui/ux update

--- workspace/javaweb/src/main/webapp/reactjs/src/Products.js
+++ workspace/javaweb/src/main/webapp/reactjs/src/Products.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'; 
 import { Link } from 'react-router-dom';
-import { Carousel, Col, Row, Modal, Button} from "react-bootstrap";
+import { Carousel, Col, Row, Button, Nav  } from "react-bootstrap";
+import $ from "jquery";
 
 import  './css/Products.css';
 
 const Products = () => { 
   
     const [products, setProducts] = useState([]);
+    const BUTTONS = ['기본'];
 
     useEffect(() => {
         fetch("/products")
@@ -17,6 +19,47 @@ const Products = () => {
                 setProducts(data);
             });
     }, []);
+
+    const handleClose = () => {
+        window.location = '/';
+    }
+
+    // function moveScrollRight() {
+    //     var _scrollX = $('.container').scrollLeft();
+    //     $('.container').scrollLeft(_scrollX + 50);
+    // };
+    const moveScrollRight = () => {
+        let scrollX = document.getElementById('container').scrollLeft;
+        document.getElementById('container').scrollLeft = scrollX + 170;
+    }
+
+    const moveScrollLeft = () =>  {
+        let scrollX = document.getElementById('container').scrollLeft;
+        document.getElementById('container').scrollLeft = scrollX - 170;
+    };
+
+    function Navigation() {
+        return  (
+            <div class="grid">
+            <div class="controller">
+                <button class="controller-b" onClick={moveScrollLeft}>◀</button>
+            </div> 
+            <div class="category-box" id="container">
+                <button className='category'>과자</button>
+                <button className='category'>사탕</button>
+                <button className='category'>음료</button>
+                <button className='category'>행사</button>
+                <button className='category'>과일</button>
+                <button className='category'>채소</button>
+                <button className='category'>냉장</button>
+                <button className='category'>정육</button>
+            </div>
+            <div class="controller">
+                <button class="controller-a" onClick={moveScrollRight}>▶</button>
+            </div>
+        </div>
+        );
+    }
     
     return ( 
         <div className="Products"> 
@@ -36,14 +79,16 @@ const Products = () => {
                     />
                 </Carousel.Item>
             </Carousel>
-            <div className="category-box">
-                <div className="category">
-                    <button className="scrollbtn">◀</button>
-                    <div>과자</div>
-                    <div>캔디</div>
-                    <div>음료</div>
-                    <div>행사</div>
-                    <button className="scrollbtn">▶</button>
+            <Navigation/>
+            <div className="tools">
+                <div>
+                    <Button variant='success' style={{fontSize:"1.3vh"}}>바코드 검색</Button>
+                </div>
+                <div>
+                    <form id="form" className="search" method='get'>
+                        <input type="text" id="search" name="search" placeholder='Search'/>
+                        <input type="submit" value="Search"/>
+                    </form>
                 </div>
             </div>
             <div className="products-box">
@@ -70,6 +115,9 @@ const Products = () => {
                         </Row>
                     </Carousel.Item>
                 </Carousel>
+            </div>
+            <div className='nav_close'>
+                <Button variant="secondary" onClick={handleClose} style={{ fontSize:"1.5vh"}}>메인으로</Button>
             </div>
         </div>
     ); 

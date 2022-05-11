3821bdce0b30437833bbb00b436c1a61dab1eb85
plainphw <s2028@e-mirim.hs.kr>
2022-05-11 09:57:44 +0900

    환불&교환 페이지 생성

--- workspace/javaweb/src/main/webapp/reactjs/src/App.js
+++ workspace/javaweb/src/main/webapp/reactjs/src/App.js
@@ -9,6 +9,7 @@ import Main from './Main';
 import Products from './Products';
 import Product from './Product';
 import Buy from './Buy';
+import Permute from './Permute';
 import NotFound from './NotFound';
 import Footer from './Footer';
 
@@ -41,6 +42,7 @@ function App() {
             <Route path='/products' element={<Products />}></Route>
             <Route path='/product/:productId' element={<Product />}></Route>
             <Route path='/buy' element={<Buy />}></Route>
+            <Route path='/permute' element={<Permute />}></Route>
             {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
             <Route path="*" element={<NotFound />}></Route>
         </Routes>

f289c6cd3fd135859407fef7150648ca008dbbb9
plainphw <s2028@e-mirim.hs.kr>
2022-05-30 14:52:01 +0900

    Node X React 연동

---
+++ customer/app/client/src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import { Link } from 'react-router-dom';
+import './css/Main.css';
+
+const Main = () => { 
+
+    return ( 
+        <div className="main">
+            <h1>Hi~!!</h1>
+        </div> 
+    ); 
+}; 
+
+export default Main;

47c10b13f92240187e9ea2f2bc2ce2b8b2661b2c
plainphw <s2028@e-mirim.hs.kr>
2022-06-07 10:28:05 +0900

    파일 정리

--- workspace/javaweb/src/main/webapp/reactjs/src/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'; 
-
-const NotFound = () => { 
-    return ( 
-        <h1 style={{height:"100vh", marginTop:"40vh"}}> 404 Error </h1> 
-    ); 
-}; 
-
-export default NotFound;

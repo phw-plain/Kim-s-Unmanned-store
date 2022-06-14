7ee724e6006fcfc71bb102198853c6d0deb7f1ab
박혜원 <79081836+phw-plain@users.noreply.github.com>
2022-06-14 22:49:23 +0900

    #18 상품 목록 페이징 처리(1/3)

--- customer/app/client/src/js/NotFound.js
+++ customer/app/client/src/js/NotFound.js
@@ -1,8 +1,12 @@
 import React from 'react'; 
 
+import { Posts } from '../component/Posts.jsx'
+
 const NotFound = () => { 
     return ( 
-        <h1 style={{height:"100vh", marginTop:"40vh"}}> 404 Error </h1> 
+        // <h1 style={{height:"100vh", marginTop:"40vh"}}> 404 Error </h1> 
+        <Posts/>
+        
     ); 
 }; 
 

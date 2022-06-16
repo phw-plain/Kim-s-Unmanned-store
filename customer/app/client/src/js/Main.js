6f4662d5930446672fef6239838d2c3571ca5252
박혜원 <79081836+phw-plain@users.noreply.github.com>
2022-06-16 23:59:31 +0900

    #18 이미지 수정
    - 점장용 프론트엔드 용 데이터 변경
    - 점장용 홈 버튼 이미지 변경

--- customer/app/client/src/js/Main.js
+++ customer/app/client/src/js/Main.js
@@ -19,14 +19,14 @@ const Main = () => {
                     <button className="menu m1 p3 shadow">
                         <div className="f3 left orange">01</div>
                         <div className="f2 left">상품 검색</div>
-                        <img className="menu_img" src="https://cdn-icons-png.flaticon.com/512/2250/2250295.png"></img>
+                        <img className="menu_img" src='/img/search.png'></img>
                     </button>
                 </Link>
                 <Link to="/buy">
                     <button className="menu m1 p3 shadow">
                         <div className="f3 left green">02</div>
                         <div className="f2 left">계산 하기</div>
-                        <img className="menu_img" src="https://cdn-icons-png.flaticon.com/512/2250/2250295.png"></img>
+                        <img className="menu_img" src='/img/buy.png'></img>
                     </button>
                 </Link> 
             </div>
@@ -35,14 +35,14 @@ const Main = () => {
                     <button className="menu m1 p3 shadow">
                         <div className="f3 left green">03</div>
                         <div className="f2 left">환불&교환</div>
-                        <img className="menu_img" src="https://cdn-icons-png.flaticon.com/512/2250/2250295.png"></img>
+                        <img className="menu_img" src='/img/permute.png'></img>
                     </button>
                 </Link>     
                 <Link to="/rank">
                     <button className="menu m1 p3 shadow">
                         <div className="f3 left orange">04</div>
                         <div className="f2 left">인기 순위</div>
-                        <img className="menu_img" src="https://cdn-icons-png.flaticon.com/512/2250/2250295.png"></img>
+                        <img className="menu_img" src='/img/rank.png'></img>
                     </button>
                 </Link>    
             </div>
